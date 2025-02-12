c361c28000365c8caf5032e954fe86c9d7807696
Sergio Yucra <sergio.yucra.btsoft@gmail.com>
2025-02-12 10:13:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 Mi Proyecto con React y Vite</h1>
+      <p>¡Todo listo para empezar!</p>
+    </div>
+  );
+}
+
+export default App;
+
