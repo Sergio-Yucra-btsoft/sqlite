94dbcc40386d84453c8a8f2900054b895cdb16a0
Sergio Yucra <sergio.yucra.btsoft@gmail.com>
2025-02-13 11:12:05 -0400

    electron

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     port: 3000,
   },
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
